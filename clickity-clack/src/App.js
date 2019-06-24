67155dc74cf181ca8f6f7aeb22e85d6dfd740461
willow <shaneweth77@gmail.com>
2019-06-24 14:46:43 -0400

    more structure and some logic

--- clickity-clack/src/App.js
+++ clickity-clack/src/App.js
@@ -1,28 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Tile from './components/Tile';
 import TileWrap from './components/TileWrap';
 import Header from './components/Header';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { gameScore: 0 }
 }
+
 
 export default App;
---
+++ clickity-clack/src/components/Tile/Tile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Tile.css'
+
+const Tile = props => (
+    <div className="tileBody">
+        <h1></h1>
+
+    </div>
+)
